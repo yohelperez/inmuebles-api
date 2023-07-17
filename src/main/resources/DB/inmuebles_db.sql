d592a3c5bf6de1a98e18ce58d5ff1dac98114846
yohelperez <perezyohel@gmail.com>
2023-07-17 08:53:24 -0500

    changes on db script

--- src/main/resources/DB/inmuebles_db.sql
+++ src/main/resources/DB/inmuebles_db.sql
@@ -12,6 +12,9 @@ CREATE TABLE persona(
 CREATE TABLE inmueble(
 	id int NOT NULL auto_increment,
 	direccion VARCHAR(100) NOT NULL, 
+    ciudad VARCHAR(50) NOT NULL, 
+    departamento VARCHAR(50) NOT NULL, 
+    barrio VARCHAR(50) NOT NULL, 
     PRIMARY KEY (id)
     
 );
@@ -21,45 +24,46 @@ CREATE TABLE contrato(
     idInmueble INT NOT NULL,
     numeroContrato INT NOT NULL,
     fecha DATE NOT NULL,
+    estado VARCHAR(50) NOT NULL,
     CONSTRAINT pk_contrato PRIMARY KEY (numeroContrato, idPersona, idInmueble),
     FOREIGN KEY (idPersona) REFERENCES persona(id)  ON DELETE CASCADE,
     FOREIGN KEY (idInmueble) REFERENCES inmueble(id)  ON DELETE CASCADE
 );
 
 
+INSERT INTO persona(nombre, apellido, correo)
+VALUES('Yohel', 'Perez', 'perezyohel@gmail.com');
 
+INSERT INTO persona(nombre, apellido, correo)
+VALUES('Juan', 'Diaz', 'juan.diaz@gmail.com');
 
-INSERT INTO barber_appointment_db.barber(email, name, phone, time_in, time_out)
-VALUES('pepitoperez@gmail.com', 'pepito perez', '3006005252', '8:00', '17:00');
+INSERT INTO persona(nombre, apellido, correo)
+VALUES('Camila', 'Botero', 'cami.botero@gmail.com');
 
 INSERT INTO inmueble(direccion, ciudad, departamento, barrio)
-VALUES('cararera 42 #31-10', 'Envigado', 'Antioquia', 'El mesa');
+VALUES('cararera 42 #31-10', 'Envigado', 'Antioquia', 'Mesa');
 
-UPDATE inmueble
-SET ciudad= 'Medellin', departamento= 'Antioquia', barrio = 'Robledo'
-WHERE id= '1';
+INSERT INTO inmueble(direccion, ciudad, departamento, barrio)
+VALUES('cararera 41 F #20-21', 'Bello', 'Antioquia', 'Cabañas');
 
 
--- contrato
+INSERT INTO contrato(idPersona, idInmueble, fecha, estado)
+VALUES( 1, 1, '2021-06-11', 'Vigente');
 
-UPDATE contrato
-SET estado= 'Vigente'
-WHERE numeroContrato= '3';
+INSERT INTO contrato(idPersona, idInmueble, fecha, estado)
+VALUES( 2, 2, '2018-03-21', 'No Vigente');
 
-ALTER TABLE contrato
-add estado varchar(50) NOT NULL;
+INSERT INTO contrato(idPersona, idInmueble, fecha, estado)
+VALUES( 3, 2, '2019-08-16', 'Vigente');
 
-INSERT INTO contrato(idPersona, idInmueble, fecha)
-VALUES( 2, 1, '2021-09-12');
 
-SELECT * FROM contrato;
 
-SELECT p.nombre, p.apellido, p.correo, i.direccion, i.ciudad, i.departamento, i.barrio, c.fecha
+/*SELECT p.nombre, p.apellido, p.correo, i.direccion, i.ciudad, i.departamento, i.barrio, c.fecha, c.estado
 FROM persona as p
 INNER JOIN contrato as c on c.idPersona = p.id
-INNER JOIN inmueble as i on i.id = c.idInmueble; 
-
-desc contrato;
+INNER JOIN inmueble as i on i.id = c.idInmueble
+WHERE p.nombre LIKE 'Yohel' OR p.apellido LIKE 'Yohel' OR p.correo LIKE 'Yohel';
+*/
 
 
 
